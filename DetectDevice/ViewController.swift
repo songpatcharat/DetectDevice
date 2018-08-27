3d123c5a872e18f619f135d3c4087f37f4ce89ed
Patcharat <Patcharat@lannasoftworks.com>
2018-08-27 14:31:41 +0700

    Initial Commit

---
+++ DetectDevice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetectDevice
+//
+//  Created by Topvalue-MACAIR2 on 27/8/2561 BE.
+//  Copyright © 2561 Topvalue-MACAIR2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
