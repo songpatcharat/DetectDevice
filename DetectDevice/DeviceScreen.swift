538adb4db0944bd243c833eb01547cf41b1ffd22
Patcharat <Patcharat@lannasoftworks.com>
2018-08-27 14:58:59 +0700

    first commit

---
+++ DetectDevice/DeviceScreen.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceScreen.swift
+//  DetectDevice
+//
+//  Created by Topvalue-MACAIR2 on 27/8/2561 BE.
+//  Copyright © 2561 Topvalue-MACAIR2. All rights reserved.
+//
+
+import Foundation
